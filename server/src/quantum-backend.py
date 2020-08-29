d52b52cab48b67b0618bca97ecb1ff1a91c48e6b
U-DESKTOP-VRI8T2L\manya <gmanya023@gmail.com>
2020-08-30 00:48:35 +0300

    updates to the game - comments added, superposition fixed

--- server/src/quantum-backend.py
+++ server/src/quantum-backend.py
@@ -1,10 +1,15 @@
-import qiskit as q
 import quantumrandom
-import math
+import qiskit as q
 
-# running a quantum simulator locally aka without internet
-from qiskit import Aer    # simulator framework for qiskit
+# running a quantum simulator locally
+# Aer is simulator framework for qiskit
+from qiskit import Aer
 from qiskit import IBMQ
+
+IBMQ.save_account(open("keypath.txt","r").read())
+IBMQ.load_account()
+backend = Aer.get_backend("qasm_simulator") # Running jobs on the simulator locally
+
 
 class Quanticorn:
     def __init__(self, n_tiles, n_lightning_bolts):
@@ -12,11 +17,17 @@ class Quanticorn:
         self.lightning_bolts = n_lightning_bolts
         self.grid = None
         self.player_grid = None
-        self.dict_of_mappings = {}
-        # create a quantum circuit with equal number of qubits and bits
-        self.circuit = q.QuantumCircuit(self.tiles*self.tiles, self.tiles*self.tiles)
+        self.dict_of_mappings = {}  # dictionary for tile to number mappings
+
+        # creates a quantum circuit with equal number of qubits and bits
+        # Note there are self.tiles*self.tiles number of tiles on board
+        # The circuit is created with equal number of qubits and bits
+        self.circuit = q.QuantumCircuit((self.tiles*self.tiles), (self.tiles*self.tiles))
 
 
+    """This function assigns a unique number to each tile in the grid
+    The mappings between is the tile position and the unique number is stored
+    in a dictionary - self.dict_of_mappings. """
     def assign_tile_numbers(self):
         list_of_tiles = []
         x = 0
@@ -27,66 +38,73 @@ class Quanticorn:
                 self.dict_of_mappings[list_of_tiles[i][j]] = [i, j]
                 x += 1
 
-    # create an empty self.grid
+
+    """This function creates a grid of tiles. It also assumes the tiles behaves
+    like qubits and places the qubits (i.e. tiles) in superposition.
+    The function uses the quantumrandom module to place lightning bolts in random
+    places on the grid. It also maps tile around each lightning bolt with a number
+    indicating how many lightning bolts are present around it. """
     def initialise_grid(self):
+        # create an empty grid
         self.grid = [[0 for row in range(self.tiles)] for column in range(self.tiles)]
 
-        # assign numbers to these tiles
+        # calls the function that assigns unique numbers to each tile
         self.assign_tile_numbers()
+
+        # put all tiles in superposition
+        for tile_number, tile in self.dict_of_mappings.items():
+            # tile_number is assumed to be equal to qubit index in this case
+            # puts the qubit in superposition by applying the hadamard gate
+            self.circuit.h(tile_number)
+
+        # generate a random position using the quantumrandom python module
+        pos_x = int(quantumrandom.randint(0, self.tiles))  # position along the row x
+        pos_y = int(quantumrandom.randint(0, self.tiles))  # position along the column y
+        self.grid[pos_x][pos_y] = 'U'                      # assign unicorn a random position
 
         # place lightning_bolts randomly
         lightning_bolts_placed = 0
         while (lightning_bolts_placed < self.lightning_bolts):
 
-            # generate a random position using the quantumrandom python module
-            pos_x = int(quantumrandom.randint(0, self.tiles))  # position along the row x
-            pos_y = int(quantumrandom.randint(0, self.tiles))  # position along the column y
+            pos_x = int(quantumrandom.randint(0, self.tiles))
+            pos_y = int(quantumrandom.randint(0, self.tiles))
             # print(pos_x, pos_y)
 
-            if (self.grid[pos_x][pos_y] != 'X'):
+            if (self.grid[pos_x][pos_y] != 'X' and self.grid[pos_x][pos_y] != 'U'):
                 self.grid[pos_x][pos_y] = 'X'
-
-                # make this part of the code more efficient ***
-                for tile_number, tile in self.dict_of_mappings.items():
-                    if tile == [pos_x, pos_y]:
-                        # print(tile_number)
-                        # tile_number is assumed to be equal to tile in this case
-                        # put the qubit in superposition by applying the hadamard gate
-                        self.circuit.h(tile_number)
-
 
                 # index tiles around the lightning_bolts
                 # Make this part of the code more efficient ***
                 if (pos_x <= self.tiles-2):
-                    if (self.grid[pos_x+1][pos_y] != 'X'):
+                     if (self.grid[pos_x+1][pos_y] != 'X' and self.grid[pos_x+1][pos_y] != 'U'):
                         self.grid[pos_x+1][pos_y] += 1  # the row after the lightning_bolt
 
                 if (pos_x >= 1):
-                    if (self.grid[pos_x-1][pos_y] != 'X'):
+                    if (self.grid[pos_x-1][pos_y] != 'X' and self.grid[pos_x-1][pos_y] != 'U'):
                         self.grid[pos_x-1][pos_y] += 1 # the row before the lightning_bolt
 
                 if (pos_y >= 1):
-                    if (self.grid[pos_x][pos_y-1] != 'X'):
+                    if (self.grid[pos_x][pos_y-1] != 'X' and self.grid[pos_x][pos_y-1] != 'U'):
                         self.grid[pos_x][pos_y-1] += 1 # the col before the lightning_bolt
 
                 if (pos_y <= self.tiles-2):
-                    if (self.grid[pos_x][pos_y+1] != 'X'):
+                    if (self.grid[pos_x][pos_y+1] != 'X' and self.grid[pos_x][pos_y+1] != 'U'):
                         self.grid[pos_x][pos_y+1] += 1 # the col after the lightning_bolt
 
                 if (pos_y <= self.tiles-2 and pos_x <= self.tiles-2):
-                    if (self.grid[pos_x+1][pos_y+1] != 'X'):
+                    if (self.grid[pos_x+1][pos_y+1] != 'X' and self.grid[pos_x+1][pos_y+1] != 'U'):
                         self.grid[pos_x+1][pos_y+1] += 1 # diagonally to the bottom right
 
                 if (pos_y <= self.tiles-2 and pos_x >= 1):
-                    if (self.grid[pos_x-1][pos_y+1] != 'X'):
+                    if (self.grid[pos_x-1][pos_y+1] != 'X' and self.grid[pos_x-1][pos_y+1] != 'U'):
                         self.grid[pos_x-1][pos_y+1] += 1 # diagonally to the bottom left
 
                 if (pos_y >= 1 and pos_x >= 1):
-                    if (self.grid[pos_x-1][pos_y-1] != 'X'):
+                    if (self.grid[pos_x-1][pos_y-1] != 'X' and self.grid[pos_x-1][pos_y-1] != 'U'):
                         self.grid[pos_x-1][pos_y-1] += 1 # diagonally to the top left
 
                 if (pos_y >= 1 and pos_x <= self.tiles-2):
-                    if (self.grid[pos_x+1][pos_y-1] != 'X'):
+                     if (self.grid[pos_x+1][pos_y-1] != 'X' and self.grid[pos_x+1][pos_y-1] != 'U'):
                         self.grid[pos_x+1][pos_y-1] += 1 # diagonally to the top right
 
                 lightning_bolts_placed += 1
@@ -94,41 +112,58 @@ class Quanticorn:
         return(self.grid)
 
 
+    """This function creates a grid for the user with tiles that have not yet been
+    flipped."""
     def initialise_player_grid(self):
         self.player_grid = [['-' for row in range(self.tiles)] for col in range(self.tiles)]
         return self.player_grid
 
 
+    """This function takes in player grid or grid as its input and prints the
+    grid on the screen."""
     def display_grid(self, grid):
         for row in grid:
             print(row)
 
-    def status(self, player_grid):
-        #########################
-        # redo this code to check for if all tiles other than lightning_bolts have been found
+
+    """This function checks if the game is over/ the player has lost."""
+    def game_finished(self, player_grid, avaliable_flips, lightning_bolt_found):
+        if avaliable_flips < 0:
+            print("Maximum limit of flips reached!")
+            return True
+
+        if (lightning_bolt_found == True):
+            print("Oops, you accidentally clicked on the lightning bolt!")
+            return True
+
         for row in player_grid:
             for tile in row:
                 if tile == '-':
                     return False
-        return True
-
-    def game_won(self, grid):
-        pass
-        #########################
-        # two scenarios - a lightning_bolt was clicked on or a unicorn was found
+        return False
 
 
-# create an instance of the game
+    """This function checks if the game has finished - either when the unicorn has been
+    found or the score of the player indicates that all tiles other than the tiles
+    with lightning bolts have been flipped."""
+    def won(self, score, unicorn_found, check_game):
+        # all tiles other than the lightning bolts were opened
+        # Note - find a better method to check for this *****
+        if (check_game == True and score == (self.tiles*self.tiles - self.lightning_bolts - 1)):
+            print("You were successful in finding the lightning bolts!")
+            return True
+        # The unicorn has been found - game won
+        elif (unicorn_found == True):
+            print("Congrats, you found the unicorn!")
+            return True
+        else:
+            return False
+
+
+
 if __name__ == "__main__":
-    IBMQ.save_account(open("keypath.txt","r").read())
-    # IBMQ.save_account('', overwrite=True)
-    IBMQ.load_account()
-    # provider = IBMQ.get_provider("ibm-q")
-    # Running jobs on the simulator
-    backend = Aer.get_backend("qasm_simulator")
-
-    score = 0
-    game = Quanticorn(3, 3)
+    game = Quanticorn(3, 4)  # create an instance of the game
+    score = 0                # inital score of the user
 
     grid = game.initialise_grid()
     game.display_grid(grid)  # for testing purposes
@@ -136,34 +171,81 @@ if __name__ == "__main__":
     player_grid = game.initialise_player_grid()
     game.display_grid(player_grid)
 
-    while game.status(player_grid) == False:
+    avaliable_flips = (game.tiles*game.tiles - 4)  # limit the number of avaliable flips
+    lightning_bolt_found = False
+    unicorn_found = False
+    check_game = False
 
+    while True:
+        game_over = game.game_finished(player_grid, avaliable_flips, lightning_bolt_found)
+        game_won = game.won(score, unicorn_found, check_game)
+
+        if(game_won == True):
+            print("YOU WON")
+            break
+
+        elif (game_over == True):
+            print("YOU LOST")
+            print("Your Score: ", score)
+            break
+
+        # Ask for user input
         print("Open a tile")
+        check = input("y to check game or n")
+        if (check == 'y'):
+            game_won = game.won(score, unicorn_found, check_game)
+            continue
         x = input("Row position 1, 2 or 3 :")
         y = input("Column position 1, 2 or 3 :")
         x = int(x) - 1 # 0 based indexing
         y = int(y) - 1 # 0 based indexing
+        # while tiles in list of opened tiles try again
 
-        # check if the player opened a tile with a lightning_bolt
-        if grid[x][y] == 'X':
+        avaliable_flips -= 1
+
+        # Check if the tile has a Unicorn
+        if grid[x][y] == 'U':
+            unicorn_found = True
+            continue
+
+        # Check if the tile has a lightning_bolt
+        elif grid[x][y] == 'X':
+
             # measure the value of the lightning_bolt which was initially put into superposition
-            game.circuit.measure([0, 1], [0, 1])
+            game.circuit.measure([0], [0])
             job = q.execute(game.circuit, backend=backend, shots=500)
-            result = job.result()
-            counts = result.get_counts(game.circuit)
+            # counts is a dictionary
+            counts = job.result().get_counts(game.circuit)
+            print("Counts ", counts)
+
             max_val = max(counts, key=counts.get)
-            if max_val == 1:
+
+            # 50 percent chance
+            if max_val != 1:
                 game.display_grid(grid)
-                print("Game Over")
-                print("Your Score: ", score)
+                lightning_bolt_found = True
+                continue
+
             else:
-                print("You just got lucky!")
-                print("The unicorn is safe. Continue playing...")
+                print("The Quantum World is in your favour!")
+                print("Continue playing...")
                 game.player_grid[x][y] = game.grid[x][y]
                 game.display_grid(player_grid)
 
+        # else if the tile is neither a unicorn nor a lightning bolt
         else:
-            # reveal the cell
-            game.player_grid[x][y] = game.grid[x][y]
+            # Choose to reveal the value on the tile is random
+            game.circuit.measure([0], [0])
+            job = q.execute(game.circuit, backend=backend, shots=500)
+            counts = job.result().get_counts(game.circuit)
+            max_val = max(counts, key=counts.get)
+
+            if max_val != 1:
+                # reveal the cell
+                game.player_grid[x][y] = game.grid[x][y]
+            else:
+                print("The Quantum World is not in your favour!")
+                game.player_grid[x][y] = '$'  # blank cell
+
             game.display_grid(player_grid)
             score += 1
